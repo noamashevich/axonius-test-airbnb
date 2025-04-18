17b6cd8499e53f1cfc1e8bb5edb1924f47a10159
Noa Mashevich <Noa.Mashevich@gmail.com>
2025-04-19 02:19:46 +0300

    bug fix

--- utils/playwright_helpers.py
+++
@@ -1,8 +0,0 @@
-def safe_get_text(page, selector: str) -> str:
-    try:
-        element = page.locator(selector)
-        if element.is_visible(timeout=3000):
-            return " ".join(element.inner_text().split())
-    except:
-        pass
-    return "Not Available"
